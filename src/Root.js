726ae606b5a46e3da50e3ff3ea94644a80f7b2e0
João Olivio Neto <joao.olivio@valtech.com>
2018-11-26 11:42:02 -0200

    Async middleware

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-import reduxPromise from 'redux-promise';
+import async from 'middleware/async';
 
 export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers, 
         initialState, 
-        applyMiddleware(reduxPromise)
+        applyMiddleware(async)
     );
     return (
         <Provider store={store}>
