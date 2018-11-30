8b1fcff2fda9dead7e5b189abed210792f8927ff
João Olivio Neto <joao.olivio@valtech.com>
2018-11-30 11:42:39 -0200

    Fixing import.

--- src/Root.js
+++ src/Root.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-import stateValidator from 'middleware';
+import stateValidator from 'middleware/stateValidator';
 import async from 'middleware/async';
 
 export default ({ children, initialState = {} }) => {
