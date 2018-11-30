08d42fcb98fce28abceb51d25cff970136d243ed
João Olivio Neto <joao.olivio@valtech.com>
2018-11-30 10:25:50 -0200

    Finishing with middlewares.

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
+import stateValidator from 'middleware';
 import async from 'middleware/async';
 
 export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers, 
         initialState, 
-        applyMiddleware(async)
+        applyMiddleware(async, stateValidator)
     );
     return (
         <Provider store={store}>
